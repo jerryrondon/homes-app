1bd7106faa879172155afa9b105e7b19f61ed3b4
Jerry Rondon <jrondon@cgtscorp.com>
2024-09-30 16:04:07 -0400

    MODIFICA EL HEADER EN app.component.ts
    Components in Angular - Learning Angular (Part 3)
        https://www.youtube.com/watch?v=R0nRX8jD2D0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello Universe!</h1>`,
+  template: `
+    <main>
+      <header class="brand-name">
+        <img src="/assets/logo.svg" class="brand-logo"
+        alt="logo" aria-hidden="true">
+      </header>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
